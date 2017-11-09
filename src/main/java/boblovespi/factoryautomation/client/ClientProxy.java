aba5b7b950a367fceafe3e15a720f8b3e8f387ab
boblovespi <boblovespi3.14@gmail.com>
2017-11-09 15:35:08 -0800

    got concrete working

--- src/main/java/boblovespi/factoryautomation/client/ClientProxy.java
+++ src/main/java/boblovespi/factoryautomation/client/ClientProxy.java
@@ -3,16 +3,26 @@ package boblovespi.factoryautomation.client;
 import boblovespi.factoryautomation.common.CommonProxy;
 import boblovespi.factoryautomation.common.block.FABlocks;
 import boblovespi.factoryautomation.common.item.FAItems;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 /**
  * Created by Willi on 11/8/2017.
  */
+@Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy
 {
 	@Override
 	public void RegisterRenders()
 	{
-		super.RegisterRenders();
+
+	}
+
+	@SubscribeEvent
+	public static void registerModels(ModelRegistryEvent event)
+	{
 		FAItems.RegisterRenders();
 		FABlocks.RegisterRenders();
 	}
