b8a85ed2bc02d45a3d76af25e5cef88a5991fd96
boblovespi <boblovespi3.14@gmail.com>
2017-11-09 15:53:37 -0800

    trying to add multitypes

--- src/main/java/boblovespi/factoryautomation/common/item/Ingot.java
+++ src/main/java/boblovespi/factoryautomation/common/item/Ingot.java
@@ -1,8 +1,35 @@
 package boblovespi.factoryautomation.common.item;
 
+import boblovespi.factoryautomation.common.item.types.Metals;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
 /**
  * Created by Willi on 11/9/2017.
  */
-public class Ingot
+public class Ingot extends MultiTypeItem<Metals>
+		implements FAItem, IMultiVariantItem
 {
+	public Ingot()
+	{
+		super("ingot", CreativeTabs.MATERIALS);
+	}
+
+	@Override
+	public String UnlocalizedName()
+	{
+		return "ingot";
+	}
+
+	@Override
+	public String GetMetaFilePath(int meta)
+	{
+		return null;
+	}
+
+	@Override
+	public Item ToItem()
+	{
+		return this;
+	}
 }
---
+++ src/main/java/boblovespi/factoryautomation/common/item/types/IMultiTypeEnum.java
@@ -0,0 +1,9 @@
+package boblovespi.factoryautomation.common.item.types;
+
+/**
+ * Created by Willi on 11/9/2017.
+ */
+public interface IMultiTypeEnum
+{
+	int GetId();
+}
