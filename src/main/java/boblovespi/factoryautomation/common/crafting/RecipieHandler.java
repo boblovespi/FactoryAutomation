361ef26ee9eb6e8681cd68437278bdedbb5a1c16
DaBlob <ariel.raizman@hotmail.com>
2017-11-10 13:28:23 -0800

    More trying to fix crafting but failing miserably

--- src/main/java/boblovespi/factoryautomation/common/crafting/RecipieHandler.java
+++ src/main/java/boblovespi/factoryautomation/common/crafting/RecipieHandler.java
@@ -1,10 +1,12 @@
 package boblovespi.factoryautomation.common.crafting;
 
 import boblovespi.factoryautomation.FactoryAutomation;
+import boblovespi.factoryautomation.common.block.FABlock;
 import boblovespi.factoryautomation.common.block.FABlocks;
 import boblovespi.factoryautomation.common.item.FAItem;
 import boblovespi.factoryautomation.common.item.FAItems;
 import boblovespi.factoryautomation.common.util.Log;
+import com.sun.istack.internal.NotNull;
 import com.sun.tools.javac.tree.JCTree;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -12,6 +14,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -25,18 +29,17 @@ public class RecipieHandler
     @SubscribeEvent
     public static void registerIRecipes(RegistryEvent.Register<IRecipe> event)
     {
-
+        NonNullList<Ingredient> n = NonNullList.create();
+        new ShapelessRecipes(FactoryAutomation.MODID,new ItemStack(FABlocks.concrete.ToBlock()), n.from(Ingredient.fromItem(FAItems.slag.ToItem())));
     }
     public static void RegisterCraftingRecipes()
     {
         GameRegistry.addShapelessRecipe(new ResourceLocation(FactoryAutomation.MODID,"concrete"), new ResourceLocation(""), new ItemStack(FABlocks.concrete.ToBlock()),Ingredient.fromItem(FAItems.slag.ToItem()), Ingredient.fromItem(Items.WATER_BUCKET));
         Log.getLogger().info("Registered Recipes");
-
-    }
-    public static void RegisterSmeltingRecipes()
-    {
+//Smelting
         Log.getLogger().info("Registered Smelting Recipes");
+                GameRegistry.addSmelting(new ItemStack(FAItems.slag.ToItem()), new ItemStack(FABlocks.concrete.ToBlock()),0.7f);
 
-    }
+                }
 
-}
+                }
