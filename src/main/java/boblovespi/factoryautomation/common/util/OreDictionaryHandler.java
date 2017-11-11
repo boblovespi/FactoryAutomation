f2a1a46c000113b57d95af037dd2341d429ad93c
boblovespi <boblovespi3.14@gmail.com>
2017-11-11 11:04:24 -0800

    fixed oredict names

--- src/main/java/boblovespi/factoryautomation/common/util/OreDictionaryHandler.java
+++ src/main/java/boblovespi/factoryautomation/common/util/OreDictionaryHandler.java
@@ -2,16 +2,15 @@ package boblovespi.factoryautomation.common.util;
 
 import boblovespi.factoryautomation.common.block.FABlocks;
 import boblovespi.factoryautomation.common.item.FAItems;
-import boblovespi.factoryautomation.common.item.types.Metals;
 import net.minecraft.item.ItemStack;
 
 public class OreDictionaryHandler {
 
     public static void registerOreDictionary() {
 
-        net.minecraftforge.oredict.OreDictionary.registerOre("Slag", FAItems.slag.ToItem());
-        net.minecraftforge.oredict.OreDictionary.registerOre("Concrete", FABlocks.concrete.ToBlock());
-        net.minecraftforge.oredict.OreDictionary.registerOre("Rice", FAItems.riceGrain.ToItem());
+        net.minecraftforge.oredict.OreDictionary.registerOre("slag", FAItems.slag.ToItem());
+        net.minecraftforge.oredict.OreDictionary.registerOre("concrete", FABlocks.concrete.ToBlock());
+        net.minecraftforge.oredict.OreDictionary.registerOre("rice", FAItems.riceGrain.ToItem());
         net.minecraftforge.oredict.OreDictionary.registerOre("ingotTin", new ItemStack(FAItems.ingot.ToItem(), 1, 3));
         net.minecraftforge.oredict.OreDictionary.registerOre("ingotCopper", new ItemStack(FAItems.ingot.ToItem(), 1, 2));
         net.minecraftforge.oredict.OreDictionary.registerOre("ingotBronze", new ItemStack(FAItems.ingot.ToItem(), 1, 4));
