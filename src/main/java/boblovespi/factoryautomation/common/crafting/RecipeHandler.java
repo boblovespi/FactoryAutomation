67162ca00540b6f1bd838b3f409fb7b534213564
boblovespi <boblovespi3.14@gmail.com>
2017-11-10 22:55:55 -0800

    fixed recipes

--- src/main/java/boblovespi/factoryautomation/common/crafting/RecipeHandler.java
+++ src/main/java/boblovespi/factoryautomation/common/crafting/RecipeHandler.java
@@ -3,20 +3,14 @@ package boblovespi.factoryautomation.common.crafting;
 import boblovespi.factoryautomation.FactoryAutomation;
 import boblovespi.factoryautomation.common.block.FABlocks;
 import boblovespi.factoryautomation.common.item.FAItems;
-import boblovespi.factoryautomation.common.util.Log;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.item.crafting.ShapelessRecipes;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 @Mod.EventBusSubscriber
 
@@ -28,31 +22,28 @@ public class RecipeHandler
 	@SubscribeEvent
 	public static void registerIRecipes(RegistryEvent.Register<IRecipe> event)
 	{
-		concrete = new ShapelessRecipes(FactoryAutomation.MODID,
-				new ItemStack(FABlocks.concrete.ToBlock()), NonNullList
-				.from(Ingredient
-								.fromItems(FAItems.slag.ToItem(), Items.WATER_BUCKET,
-										Item.getItemFromBlock(Blocks.SAND)),
-						Ingredient.fromItem(Items.WATER_BUCKET), Ingredient
-								.fromItem(Item.getItemFromBlock(Blocks.SAND))));
-		concrete.setRegistryName(
+		concrete = new ShapelessOreRecipe(
+				new ResourceLocation(FactoryAutomation.MODID, "concrete"),
+				FABlocks.concrete.ToBlock(), FAItems.slag.ToItem(),
+				Items.WATER_BUCKET, Blocks.SAND).setRegistryName(
 				new ResourceLocation(FactoryAutomation.MODID, "concrete"));
+
 		event.getRegistry().register(concrete);
 	}
 
 	public static void RegisterCraftingRecipes()
 	{
-		GameRegistry.addShapelessRecipe(
-				new ResourceLocation(FactoryAutomation.MODID, "concrete"),
-				new ResourceLocation(""),
-				new ItemStack(FABlocks.concrete.ToBlock()),
-				Ingredient.fromItem(FAItems.slag.ToItem()),
-				Ingredient.fromItem(Items.WATER_BUCKET));
-		Log.getLogger().info("Registered Recipes");
-		//Smelting
-		Log.getLogger().info("Registered Smelting Recipes");
-		GameRegistry.addSmelting(new ItemStack(FAItems.slag.ToItem()),
-				new ItemStack(FABlocks.concrete.ToBlock()), 0.7f);
+		//		GameRegistry.addShapelessRecipe(
+		//				new ResourceLocation(FactoryAutomation.MODID, "concrete"),
+		//				new ResourceLocation(""),
+		//				new ItemStack(FABlocks.concrete.ToBlock()),
+		//				Ingredient.fromItem(FAItems.slag.ToItem()),
+		//				Ingredient.fromItem(Items.WATER_BUCKET));
+		//		Log.getLogger().info("Registered Recipes");
+		//		//Smelting
+		//		Log.getLogger().info("Registered Smelting Recipes");
+		//		GameRegistry.addSmelting(new ItemStack(FAItems.slag.ToItem()),
+		//				new ItemStack(FABlocks.concrete.ToBlock()), 0.7f);
 
 	}
 
