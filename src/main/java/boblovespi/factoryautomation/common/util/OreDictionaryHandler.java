d88de9382a3195f3edbb50380521b2e75f30fd2b
boblovespi <boblovespi3.14@gmail.com>
2017-11-11 21:46:17 -0800

    fixed some oredict stuff

--- src/main/java/boblovespi/factoryautomation/common/util/OreDictionaryHandler.java
+++ src/main/java/boblovespi/factoryautomation/common/util/OreDictionaryHandler.java
@@ -3,25 +3,33 @@ package boblovespi.factoryautomation.common.util;
 import boblovespi.factoryautomation.common.block.FABlocks;
 import boblovespi.factoryautomation.common.item.FAItems;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
-public class OreDictionaryHandler {
+public class OreDictionaryHandler
+{
 
-    public static void registerOreDictionary() {
+	public static void registerOreDictionary()
+	{
 
-        net.minecraftforge.oredict.OreDictionary.registerOre("slag", FAItems.slag.ToItem());
-        net.minecraftforge.oredict.OreDictionary.registerOre("concrete", FABlocks.concrete.ToBlock());
-        net.minecraftforge.oredict.OreDictionary.registerOre("rice", FAItems.riceGrain.ToItem());
-        net.minecraftforge.oredict.OreDictionary.registerOre("ingotTin", new ItemStack(FAItems.ingot.ToItem(), 1, 3));
-        net.minecraftforge.oredict.OreDictionary.registerOre("ingotCopper", new ItemStack(FAItems.ingot.ToItem(), 1, 2));
-        net.minecraftforge.oredict.OreDictionary.registerOre("ingotBronze", new ItemStack(FAItems.ingot.ToItem(), 1, 4));
-        net.minecraftforge.oredict.OreDictionary.registerOre("ingotSteel", new ItemStack(FAItems.ingot.ToItem(), 1, 5));
-        net.minecraftforge.oredict.OreDictionary.registerOre("nuggetTin", new ItemStack(FAItems.nugget.ToItem(), 1, 3));
-        net.minecraftforge.oredict.OreDictionary.registerOre("nuggetCopper", new ItemStack(FAItems.nugget.ToItem(), 1, 2));
-        net.minecraftforge.oredict.OreDictionary.registerOre("nuggetBronze", new ItemStack(FAItems.nugget.ToItem(), 1, 4));
-        net.minecraftforge.oredict.OreDictionary.registerOre("nuggetSteel", new ItemStack(FAItems.nugget.ToItem(), 1, 5));
-        net.minecraftforge.oredict.OreDictionary.registerOre("bronzeAxe", FAItems.bronzeAxe.ToItem());
-        net.minecraftforge.oredict.OreDictionary.registerOre("bronzeHoe", FAItems.bronzeHoe);
+		OreDictionary.registerOre("slag", FAItems.slag.ToItem());
+		OreDictionary.registerOre("concrete", FABlocks.concrete.ToBlock());
+		OreDictionary.registerOre("rice", FAItems.riceGrain.ToItem());
+		OreDictionary.registerOre("ingotTin",
+				new ItemStack(FAItems.ingot.ToItem(), 1, 3));
+		OreDictionary.registerOre("ingotCopper",
+				new ItemStack(FAItems.ingot.ToItem(), 1, 2));
+		OreDictionary.registerOre("ingotBronze",
+				new ItemStack(FAItems.ingot.ToItem(), 1, 4));
+		OreDictionary.registerOre("ingotSteel",
+				new ItemStack(FAItems.ingot.ToItem(), 1, 5));
+		OreDictionary.registerOre("nuggetTin",
+				new ItemStack(FAItems.nugget.ToItem(), 1, 3));
+		OreDictionary.registerOre("nuggetCopper",
+				new ItemStack(FAItems.nugget.ToItem(), 1, 2));
+		OreDictionary.registerOre("nuggetBronze",
+				new ItemStack(FAItems.nugget.ToItem(), 1, 4));
+		OreDictionary.registerOre("nuggetSteel",
+				new ItemStack(FAItems.nugget.ToItem(), 1, 5));
 
-
-    }
+	}
 }
