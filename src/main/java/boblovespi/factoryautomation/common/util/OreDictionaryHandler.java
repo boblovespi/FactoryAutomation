46b3daf4fe07c6b08bb12136ed1182e6953a4eee
DaBlob <ariel.raizman@hotmail.com>
2017-11-11 21:37:50 -0800

    .

--- src/main/java/boblovespi/factoryautomation/common/util/OreDictionaryHandler.java
+++ src/main/java/boblovespi/factoryautomation/common/util/OreDictionaryHandler.java
@@ -19,7 +19,8 @@ public class OreDictionaryHandler {
         net.minecraftforge.oredict.OreDictionary.registerOre("nuggetCopper", new ItemStack(FAItems.nugget.ToItem(), 1, 2));
         net.minecraftforge.oredict.OreDictionary.registerOre("nuggetBronze", new ItemStack(FAItems.nugget.ToItem(), 1, 4));
         net.minecraftforge.oredict.OreDictionary.registerOre("nuggetSteel", new ItemStack(FAItems.nugget.ToItem(), 1, 5));
-        net.minecraftforge.oredict.OreDictionary.registerOre("copperAxe", FAItems.bronzeAxe.ToItem());
+        net.minecraftforge.oredict.OreDictionary.registerOre("bronzeAxe", FAItems.bronzeAxe.ToItem());
+        net.minecraftforge.oredict.OreDictionary.registerOre("bronzeHoe", FAItems.bronzeHoe);
 
 
     }
