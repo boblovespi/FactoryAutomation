2d9824c5a99bf42039b573f78c6653000e389b22
boblovespi <boblovespi3.14@gmail.com>
2018-07-25 00:47:54 -0700

    fixed placed bucket

--- src/main/java/boblovespi/factoryautomation/common/block/PlacedBucket.java
+++ src/main/java/boblovespi/factoryautomation/common/block/PlacedBucket.java
@@ -7,9 +7,9 @@ import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -26,13 +26,15 @@ import javax.annotation.Nullable;
 public class PlacedBucket extends Block implements ITileEntityProvider, FABlock
 {
 	public static final AxisAlignedBB AXIS_ALIGNED_BB = new AxisAlignedBB(
-			4 / 16d, 0, 4 / 16d, 10 / 16d, 8 / 16d, 10 / 16d);
+			4 / 16d, 0, 4 / 16d, 12 / 16d, 8 / 16d, 12 / 16d);
 
 	public PlacedBucket()
 	{
 		super(Material.IRON);
 		setUnlocalizedName(UnlocalizedName());
 		setRegistryName(RegistryName());
+		setHardness(0.2f);
+		setHarvestLevel("pickaxe", 0);
 		setResistance(10000);
 		FABlocks.blocks.add(this);
 		TileEntityHandler.tiles.add(TEPlacedBucket.class);
@@ -46,28 +48,6 @@ public class PlacedBucket extends Block implements ITileEntityProvider, FABlock
 	public TileEntity createNewTileEntity(World worldIn, int meta)
 	{
 		return new TEPlacedBucket();
-	}
-
-	/**
-	 * This gets a complete list of items dropped from this block.
-	 */
-	@Override
-	public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
-	{
-		TileEntity te = world.getTileEntity(pos);
-		if (te != null)
-		{
-			if (te instanceof TEPlacedBucket)
-			{
-				Fluid fluid = ((TEPlacedBucket) te).GetFluid();
-				if (fluid != null)
-				{
-					drops.add(FluidUtil.getFilledBucket(new FluidStack(fluid, Fluid.BUCKET_VOLUME)));
-					return;
-				}
-			}
-		}
-		drops.add(new ItemStack(Items.BUCKET));
 	}
 
 	@Override
@@ -92,5 +72,55 @@ public class PlacedBucket extends Block implements ITileEntityProvider, FABlock
 	public boolean isOpaqueCube(IBlockState state)
 	{
 		return false;
+	}
+
+	/**
+	 * @return true if the state occupies all of its 1x1x1 cube
+	 */
+	@Override
+	public boolean isFullBlock(IBlockState state)
+	{
+		return false;
+	}
+
+	/**
+	 * Return true if the block is a normal, solid cube.  This
+	 * determines indirect power state, entity ejection from blocks, and a few
+	 * others.
+	 *
+	 * @param state The current state
+	 * @param world The current world
+	 * @param pos   Block position in world
+	 * @return True if the block is a full cube
+	 */
+	@Override
+	public boolean isNormalCube(IBlockState state, IBlockAccess world, BlockPos pos)
+	{
+		return false;
+	}
+
+	/**
+	 * Called serverside after this block is replaced with another in Chunk, but before the Tile Entity is updated
+	 */
+	@Override
+	public void breakBlock(World world, BlockPos pos, IBlockState state)
+	{
+		TileEntity te = world.getTileEntity(pos);
+		if (te != null)
+		{
+			if (te instanceof TEPlacedBucket)
+			{
+				Fluid fluid = ((TEPlacedBucket) te).GetFluid();
+				if (fluid != null)
+				{
+					InventoryHelper.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(),
+							FluidUtil.getFilledBucket(new FluidStack(fluid, Fluid.BUCKET_VOLUME)));
+					super.breakBlock(world, pos, state);
+					return;
+				}
+			}
+		}
+		InventoryHelper.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Items.BUCKET));
+		super.breakBlock(world, pos, state);
 	}
 }
