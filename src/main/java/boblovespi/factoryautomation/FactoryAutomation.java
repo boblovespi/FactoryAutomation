93dc6e27d32f52b8ac9d2b0b020daa70355bd3cf
boblovespi <boblovespi3.14@gmail.com>
2018-07-28 06:07:12 -0700

    version change

--- src/main/java/boblovespi/factoryautomation/FactoryAutomation.java
+++ src/main/java/boblovespi/factoryautomation/FactoryAutomation.java
@@ -36,7 +36,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class FactoryAutomation
 {
 	public static final String MODID = "factoryautomation";
-	public static final String VERSION = "alpha 0.0.6";
+	public static final String VERSION = "alpha 0.0.7";
 	public static final String NAME = "Factory Automation";
 	public static final String SERVER_PROXY_CLASS = "boblovespi.factoryautomation.common.ServerProxy";
 	public static final String CLIENT_PROXY_CLASS = "boblovespi.factoryautomation.client.ClientProxy";
