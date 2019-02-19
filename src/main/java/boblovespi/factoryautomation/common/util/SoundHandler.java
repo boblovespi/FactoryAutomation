2503e49ed4333f9936e4ba4848994f311dc75caf
boblovespi <boblovespi3.14@gmail.com>
2019-02-18 22:01:04 -0800

    made sounds working

--- src/main/java/boblovespi/factoryautomation/common/util/SoundHandler.java
+++ src/main/java/boblovespi/factoryautomation/common/util/SoundHandler.java
@@ -1,16 +1,21 @@
 package boblovespi.factoryautomation.common.util;
 
 import net.minecraft.block.SoundType;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.event.RegistryEvent;
 
-public class SoundHandler {
+public class SoundHandler
+{
 
 	// Rock
 	public static SoundEvent breakRock = new SoundEvent(new ResourceLocation("factoryautomation", "break_rock"));
-	public static SoundType rock = new SoundType(1f, 1f, breakRock, null, null, null, null);
+	public static SoundType rock = new SoundType(1f, 1f, breakRock, SoundEvents.BLOCK_STONE_STEP,
+			SoundEvents.BLOCK_STONE_PLACE, SoundEvents.BLOCK_STONE_HIT, SoundEvents.BLOCK_STONE_FALL);
 
-	public static void register() {
-		SoundEvent.registerSounds();
+	public static void RegisterSounds(RegistryEvent.Register<SoundEvent> event)
+	{
+		event.getRegistry().register(breakRock);
 	}
 }
