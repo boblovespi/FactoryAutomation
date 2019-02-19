afa6b21bda8863bda5135840017789fe9cb915ec
boblovespi <boblovespi3.14@gmail.com>
2019-02-18 23:40:20 -0800

    subscribed to event

--- src/main/java/boblovespi/factoryautomation/common/util/SoundHandler.java
+++ src/main/java/boblovespi/factoryautomation/common/util/SoundHandler.java
@@ -5,7 +5,10 @@ import net.minecraft.init.SoundEvents;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Mod.EventBusSubscriber
 public class SoundHandler
 {
 
@@ -14,6 +17,7 @@ public class SoundHandler
 	public static SoundType rock = new SoundType(1f, 1f, breakRock, SoundEvents.BLOCK_STONE_STEP,
 			SoundEvents.BLOCK_STONE_PLACE, SoundEvents.BLOCK_STONE_HIT, SoundEvents.BLOCK_STONE_FALL);
 
+	@SubscribeEvent
 	public static void RegisterSounds(RegistryEvent.Register<SoundEvent> event)
 	{
 		event.getRegistry().register(breakRock);
