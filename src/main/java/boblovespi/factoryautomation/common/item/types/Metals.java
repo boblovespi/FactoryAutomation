792011203a9ec13450a87e514b609a7ea100d5ff
boblovespi <boblovespi3.14@gmail.com>
2021-11-23 00:34:11 -0800

    more properties of metals

--- src/main/java/boblovespi/factoryautomation/common/item/types/Metals.java
+++ src/main/java/boblovespi/factoryautomation/common/item/types/Metals.java
@@ -12,27 +12,40 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @MethodsReturnNonnullByDefault
 public enum Metals implements IStringSerializable, IMultiTypeEnum
 {
-	IRON(0, "iron", 1538, 0xFFEAEEF2),
-	GOLD(1, "gold", 10000, 0xFFFAF437),
-	COPPER(2, "copper", 1084, 0xFFFF973D),
-	TIN(3, "tin", 232, 0xFFF7E8E8),
-	BRONZE(4, "bronze", 950, 0xFFFFB201),
-	STEEL(5, "steel", 10000, 0xFF000000),
-	PIG_IRON(6, "pig_iron", 10000, 0xFF000000),
-	MAGMATIC_BRASS(7, "magmatic_brass", 10000, 0xFF000000),
-	SILVER(8, "silver", 10000, 0xFF000000),
-	LEAD(9, "lead", 10000, 0xFF000000);
+	IRON(0, "iron", 1538, 0xFFEAEEF2, 447, 7870),
+	GOLD(1, "gold", 10000, 0xFFFAF437, 129, 19300),
+	COPPER(2, "copper", 1084, 0xFFFF973D, 385, 8933),
+	TIN(3, "tin", 232, 0xFFF7E8E8, 7310, 227),
+	BRONZE(4, "bronze", 950, 0xFFFFB201, 355, 8780),
+	STEEL(5, "steel", 10000, 0xFF000000, 434, 7854),
+	PIG_IRON(6, "pig_iron", 10000, 0xFF000000, 415, 7850),
+	MAGMATIC_BRASS(7, "magmatic_brass", 10000, 0xFF000000, 0, 0),
+	SILVER(8, "silver", 10000, 0xFF000000, 235, 10500),
+	LEAD(9, "lead", 10000, 0xFF000000, 129, 11340);
 	private final int id;
 	private final String name;
 	public final int meltTemp;
 	public final int color;
+	public final float massHeatCapacity;
+	public final float density;
 
-	Metals(int id, String name, int meltTemp, int color)
+
+	/**
+	 * @param id               a unique id
+	 * @param name             the name of the metal
+	 * @param meltTemp         the temperature at which the metal melts at STP
+	 * @param color            the color of the metal
+	 * @param massHeatCapacity the mass heat capacity of the metal in J/(kg*K) source: http://cecs.wright.edu/people/faculty/sthomas/htappendix01.pdf
+	 * @param density          the density of the metal in kg/m3
+	 */
+	Metals(int id, String name, int meltTemp, int color, float massHeatCapacity, float density)
 	{
 		this.id = id;
 		this.name = name;
 		this.meltTemp = meltTemp;
 		this.color = color;
+		this.massHeatCapacity = massHeatCapacity;
+		this.density = density;
 	}
 
 	public static Metals GetFromName(String name)
